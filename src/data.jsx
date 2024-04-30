0496b910dfb10a0aa8e76f55d272c447c50e1cb9
fajri-farid <fajrijry14@gmail.com>
2024-04-30 22:10:39 +0800

    show get data to web

--- src/data.jsx
+++ src/data.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export const DataComponent = () => {
+  const [apiData, setApiData] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -8,13 +10,26 @@ export const DataComponent = () => {
         const response = await fetch(url);
         const result = await response.json();
         console.log("Data dari API:", result);
+        setApiData(result.data); // Menyimpan data dari API ke dalam state
       } catch (error) {
         console.log("Error:", error);
       }
     };
 
     fetchData();
-  }, []); // useEffect akan dipanggil hanya sekali saat komponen ini di-mount
+  }, []);
 
-  return <div>Data</div>;
+  return (
+    <div>
+      <h1>Data dari API:</h1>
+      <ul>
+        {apiData.map((item) => (
+          <li key={item._id}>
+            Nama: {item.name}, Dibuat pada:{" "}
+            {new Date(item.createdAt).toString()}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
