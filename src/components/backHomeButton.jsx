51437adc8716879483255ca416bf5ce71458f0b6
fajri-farid <fajrijry14@gmail.com>
2024-05-01 22:06:33 +0800

    testing vercel 2

--- src/components/backHomeButton.jsx
+++ src/components/backHomeButton.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export const BackHomeButton = () => {
   return (
     <div className="text-center">
-      <Link to="/">
+      <Link to="/home">
         <button>Back</button>
       </Link>
     </div>
