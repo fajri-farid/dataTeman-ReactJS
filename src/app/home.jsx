ebc4d515bf96e9247a7d51255acf3211161bc29f
fajri-farid <fajrijry14@gmail.com>
2024-05-01 12:39:30 +0800

    header done

--- src/app/home.jsx
+++ src/app/home.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { DataComponent } from "../components/data";
+import { Header } from "../components/header";
 
 export const Home = () => {
   return (
     <div>
-      <h1>Welcome to home</h1>
-      <div>
-        <DataComponent />
+      <Header />
+      <div className="main">
+        <h1>Welcome to home</h1>
+        <div>
+          <DataComponent />
+        </div>
+        <Link to="/tambah-data">
+          <button>tambah data</button>
+        </Link>
       </div>
-      <Link to="/tambah-data">
-        <button>tambah data</button>
-      </Link>
     </div>
   );
 };
---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// import header from "src/style/header.module.css";
+
+export const Header = () => {
+  return (
+    <div className="bg-indigo-900 p-3">
+      <h2 className="text-white text-center text-[20px] font-bold">
+        Data Teman
+      </h2>
+    </div>
+  );
+};
